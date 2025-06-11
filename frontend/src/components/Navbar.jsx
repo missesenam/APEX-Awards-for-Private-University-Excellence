7a0a417d4a0c924f147f4e600703e535bdb63b6e
missesenam <moneyesenam@gmail.com>
2025-06-11 16:47:19 +0100

    my commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,84 +1,91 @@
-import React, { useState } from "react";
-
+import logo from "../assets/apexLogo-removebg-preview.png";
 const Navbar = () => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
-
   return (
-    <div className="navbar md:w-[80%] mx-auto bg-background relative">
-      <div className="flex items-center justify-between w-full">
-        {/* Logo */}
-        <div className="flex-1">
-          <a href="/" className="text-xl w-[100%] sm:w-auto px-0">
-            <img
-              src="/Iogo/Foresight.png"
-              alt="Foresight Logo"
-              className="h-[40px] max-w-full"
-            />
-          </a>
-        </div>
-
-        {/* Hamburger Icon */}
-        <div className="flex-none sm:hidden">
-          <button onClick={toggleMenu} className="p-2 focus:outline-none">
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
+              className="h-5 w-5"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
+              {" "}
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16m-7 6h7"
-              />
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
             </svg>
-          </button>
-        </div>
-
-        {/* Desktop Menu */}
-        <div className="flex-none hidden sm:block opacity-70 backdrop-blur-3xl">
-          <ul className="menu menu-horizontal px-1">
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
             <li>
-              <a href="/">Home</a>
+              <a>Home</a>
             </li>
             <li>
-              <a href="/about">About</a>
+              <a>About</a>
+              <ul className="p-2">
+                <li>
+                  <a>About Us</a>
+                </li>
+                <li>
+                  <a>Category</a>
+                </li>
+                <li>
+                  <a>Process</a>
+                </li>
+              </ul>
             </li>
             <li>
-              <a href="/services">Services</a>
+              <a>Winners</a>
             </li>
             <li>
-              <a href="/contact">Contact</a>
+              <a>Event</a>
             </li>
           </ul>
         </div>
+        <a className="btn btn-ghost text-xl">
+          <img src={logo} alt="logo" className="h-16" />
+        </a>
       </div>
-
-      {/* Mobile Menu */}
-      {isMobileMenuOpen && (
-        <div className="sm:hidden absolute top-full left-0 w-full justify-end backdrop-blur-lg z-10">
-          <ul className="menu p-2">
-            <li className="border-b border-accent">
-              <a href="/">Home</a>
-            </li>
-            <li className="border-b border-accent">
-              <a href="/about">About</a>
-            </li>
-            <li className="border-b border-accent">
-              <a href="/services">Services</a>
-            </li>
-            <li className="border-b border-accent">
-              <a href="/contact">Contact</a>
-            </li>
-          </ul>
-        </div>
-      )}
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <details>
+              <summary>About</summary>
+              <ul className="p-2">
+                <li>
+                  <a>About Us</a>
+                </li>
+                <li>
+                  <a>Category</a>
+                </li>
+                <li>
+                  <a>Process</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <a>Winners</a>
+          </li>
+          <li>
+            <a>Event</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Contact</a>
+      </div>
     </div>
   );
 };
